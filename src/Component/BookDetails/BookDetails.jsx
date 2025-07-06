2a4a645ebfb4ef902cd9d29f64f7402d478c007b
rajalishikhe <nahidujjamanraj@gmail.com>
2025-07-06 23:43:30 +0600

    Now I am set the book details

--- src/Component/BookDetails/BookDetails.jsx
+++ src/Component/BookDetails/BookDetails.jsx
@@ -11,12 +11,26 @@ const BookDetails = () => {
    
     const find_data= data.find(book=>book.bookId === BookId)
     console.log(find_data)
-    
+    const {author,bookId,bookName,category,image,publisher,rating,tags,totalPages,yearOfPublishing,review}=find_data
+
     
 
     return (
         <div>
-            <h2>Welcome to my Book details Page </h2>
+            <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row">
+    <img
+      src={image}
+      className="max-w-sm rounded-lg shadow-2xl w-2/3 h-auto lg:w-[425px] lg:h-[564px] md:w-auto md:h-auto" />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">
+        {review}
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
             
         </div>
     );
