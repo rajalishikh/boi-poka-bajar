623eb77f0d3c7ded75687c5877286aef90568870
rajalishikhe <nahidujjamanraj@gmail.com>
2025-07-09 08:56:48 +0600

    Now I add a button Which display the review when I am Click

--- src/Component/BookDetails/BookDetails.jsx
+++ src/Component/BookDetails/BookDetails.jsx
@@ -26,6 +26,9 @@ const BookDetails = () => {
       saveBookName(bookName)
 
     }
+    const handleBookReview=()=>{
+      document.getElementById("review").classList.remove("hidden")
+    }
 
     return (
         <div>
@@ -36,12 +39,13 @@ const BookDetails = () => {
       className="max-w-sm rounded-lg shadow-2xl w-2/3 h-auto lg:w-[425px] lg:h-[564px] md:w-auto md:h-auto" />
     <div>
       <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">
+      <p id="review" className="py-6 hidden ">
         {review}
       </p>
       <div className="flex  gap-2">
         <button onClick={()=>handleBookId(bookId)} className="btn  hover:bg-[#50B1C9] hover:text-white">Mark asRead</button>
         <button onClick={()=>handleBookName(bookName)} className="btn hover:bg-[#50B1C9] hover:text-white"> MArk asWishList</button>
+        <button onClick={handleBookReview}  className="btn hover:bg-[#50B1C9] hover:text-white"> Book Review</button>
       </div>
     </div>
   </div>
